20d4138f418143aa375edda7db2bb64c13482db3
Xandll <128627900+Xandll@users.noreply.github.com>
2023-08-28 22:57:54 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function toggleMenu() {
+  var navbar = document.getElementById("myNavbar");
+  if (navbar.className === "navbar") {
+    navbar.className += " responsive";
+  } else {
+    navbar.className = "navbar";
+  }
+}
